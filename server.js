87f2daa3fabcb86764aead79a93f5ecbbf854428
Matt Martin <matt@enigma.local>
2020-03-08 12:26:57 -0700

    friends post working to compare scores

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const friends = require("./app/data/friends.js");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 app.use(express.static('static'));
 
@@ -28,10 +30,29 @@ app.get("/api/friends", function (req, res) {
 });
 
 app.post("/api/friends", function (req, res) {
-    res.sendFile(path.join(__dirname + "/html/", "survey.html"));
+
+    let response = req.body;
+    let scores = []
+    friends.forEach((friend, friendIndex) => {
+        let sum = 0;
+        friend.scores.forEach((friendScore, scoreIndex) => {
+            // TODO: Make positive
+            sum += friendScore - response.scores[scoreIndex];
+            //console.log(sum);
+        });
+        scores.push([{
+            "index": friendIndex,
+            "value": sum
+        }]);
+    });
+
+    // figure out the closest match
+    scores.sort((a, b) => a[0].value - b[0].value);
+    // scores[0] now has the index of the closest match;
+    // TODO: Return closests match
+    return res.json(friends[scores[0].index]);
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
-  
+});
